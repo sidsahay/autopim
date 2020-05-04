17a9d2072a972be9c9e806344bedd7c859985db3
sidsahay <siddharthsahay9700@gmail.com>
2020-05-04 12:42:36 -0400

    identification workingwq

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 clang -Xclang -disable-O0-optnone -O0 -emit-llvm -c $1.c -o $1.bc
-#opt -mem2reg $1.bc -o $1-m2r.bc
-opt -load ./pimgen.so -pimgen $1.bc -o out.bc
+opt -mem2reg $1.bc -o $1-m2r.bc
+opt -indvars $1-m2r.bc -o $1-indvars.bc
+opt -load ./pimgen.so -pimgen $1-indvars.bc -o out.bc
--- tests/loop.c
+++ tests/loop.c
@@ -1,10 +1,11 @@
-int loop(int A[100][100], int B[100][100], int C[100][100]) {
+int loop(int A[][100]) {
     int sum = 0;
 
     for (int i = 0; i < 20; i++) {
-        for (int j = 0; j < 40; j++) {
-            A[i][j] = B[i][j] + C[i][j];
-            sum += A[i][j];
+        for (int j = 0; j < 20; j++) {
+            A[i][j] += 1;
         }
     }
+
+    return sum;
 }
